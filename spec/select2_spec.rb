6f9f5307e17c04074c1a66b38d5950008e3bcb53
thalitasg <thalitasg@gmail.com>
2020-03-29 15:32:44 -0300

    inclusao de teste de combo de selecao

---
+++ spec/select2_spec.rb
@@ -0,0 +1,39 @@
+describe 'Select2', :select2 do
+        describe ('single'), :single do
+            before(:each) do
+                visit 'https://training-wheels-protocol.herokuapp.com/apps/select2/single.html' 
+            
+            end
+
+            it 'seleciona ator por nome' do  
+                find('.select2-selection--single').click
+                sleep 5 
+                find('.select2-results__option', text: 'Adam Sandler').click
+                sleep 5
+              
+
+            end   
+        
+            it 'busca e clica no ator' do  
+                find('.select2-selection--single').click
+                sleep 1 
+                find('.select2-search__field').set 'Chris Rock'
+                sleep 1
+                find('.select2-results__option').click
+                sleep 5
+            end   
+
+
+
+
+            describe('multiple') do 
+                before(:each) do    
+                end
+            end
+
+
+            after(:each) do
+                sleep 2 
+            end    
+        end
+end
